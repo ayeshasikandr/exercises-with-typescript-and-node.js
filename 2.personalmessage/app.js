f2b7b4d4532dc35108577b7e49c8f4d8831bc0fa
Ayesha Sikander <159881067+ayeshasikandr@users.noreply.github.com>
2024-02-27 10:47:34 +0500

    Add files via upload

---
+++ 2.personalmessage/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = 'eric';
+let message = 'would you like to learn some Python today?';
+console.log(`hello! ${personName} , ${message}`);
---
+++ 2.personalmessage/app.ts
@@ -0,0 +1,24 @@
+let personName :string = 'eric';
+let message :string = 'would you like to learn some Python today?';
+console.log(`hello! ${personName} , ${message}`); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
