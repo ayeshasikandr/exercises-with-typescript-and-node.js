58686167b339e18041e21935be98077e0df791d9
Ayesha Sikander <159881067+ayeshasikandr@users.noreply.github.com>
2024-02-27 12:05:20 +0500

    Add files via upload

---
+++ 6.Strriping_Names/Stripping_Names.js
@@ -0,0 +1,3 @@
+"use strict";
+const personName = '    \n\tShahid Afridi\t\n   ';
+console.log(personName);
---
+++ 6.Strriping_Names/Stripping_Names.ts
@@ -0,0 +1,2 @@
+const personName :string = '    \n\tShahid Afridi\t\n   ';
+console.log(personName);
